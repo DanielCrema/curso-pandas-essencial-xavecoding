e0cd8258ddf799316470d75f6a4a280864d858bf
DanielCrema <danielborgescrema@gmail.com>
2025-04-18 21:11:27 -0300

    printing dataset head and tail

--- manipulacao_de_dados.py
+++ manipulacao_de_dados.py
@@ -3,4 +3,6 @@ import pandas as pd
 # Loading dataset
 dataset = pd.read_csv('datasets/GasPricesinBrazil_2004-2019.csv', sep=';')
 
-print(dataset)
+# Printing dataset
+print(dataset.head())
+print(dataset.tail())
